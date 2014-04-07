3e90f09555052a8ca05ebfee953589d08b9378c8
Konstantinos Koukopoulos <koukopoulos@gmail.com>
2014-04-07 10:48:56 +0300

    add url with port for mock aws s3
    Conflicts:
    	moto/s3/urls.py
    
    Conflicts:
    	moto/s3/urls.py

--- moto/s3/urls.py
+++ moto/s3/urls.py
@@ -2,7 +2,9 @@ from __future__ import unicode_literals
 from .responses import S3ResponseInstance
 
 url_bases = [
-    "https?://(?P<bucket_name>[a-zA-Z0-9\-_.]*)\.?s3(.*).amazonaws.com"
+    "https?://(?P<bucket_name>[a-zA-Z0-9\-_.]*)\.?s3(.*).amazonaws.com",
+    "https?://(?P<bucket_name>[a-zA-Z0-9\-_.]*)\.?s3.mockaws.com",
+    "https?://(?P<bucket_name>[a-zA-Z0-9\-_.]*)\.?s3.mockaws.com:9876",
 ]
 
 url_paths = {
